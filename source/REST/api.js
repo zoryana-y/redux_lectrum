58a6c9b24a16087297c2ceb3f215973a46f48f4a
Zoryana.Yakubovich <zorianay@gmail.com>
2018-10-12 18:53:36 +0300

    homework-lesson-3

--- source/REST/api.js
+++ source/REST/api.js
@@ -1,5 +1,5 @@
 // Instruments
-import { MAIN_URL, groupId} from './config';
+import { MAIN_URL, groupId } from './config';
 
 export const api = {
     posts: {
@@ -9,6 +9,17 @@ export const api = {
                 headers: {
                     'x-no-auth': groupId,
                 },
+            });
+        },
+
+        create (comment) {
+            return fetch(`${MAIN_URL}/feed`, {
+                method:  'POST',
+                headers: {
+                    'x-no-auth':    groupId,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ comment }),
             });
         },
     },
